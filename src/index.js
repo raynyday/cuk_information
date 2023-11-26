f4d6f9c4bdfb5dadd8c1cc36b6972f0bbf42f68f
kimhyunji24 <hyunji4560@gmail.com>
2023-11-26 21:07:30 +0900

    feat. 기본 페이지 설정

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render();
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
